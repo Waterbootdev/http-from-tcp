6a42ca51bd110987a9364194be2e8b6db899b1d0
Waterbootdev <wassermann.sp@gmail.com>
2025-08-08 10:43:38 +0200

    read chunks of 8 bytes

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
-package httpfromtcp
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func readLines() {
+	file, err := os.Open("message.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	line := ""
+
+	for {
+		bs := make([]byte, 8)
+		_, err = file.Read(bs)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+
+		parts := strings.Split(string(bs), "\n")
+
+		if len(parts) > 1 {
+			line += parts[0]
+			fmt.Printf("read: %s\n", line)
+			line = parts[1]
+		} else {
+			line += parts[0]
+		}
+	}
+
+	if len(line) > 0 {
+		fmt.Printf("line: %s\n", line)
+		line = ""
+	}
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("message.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	for {
+		bs := make([]byte, 8)
+		_, err = file.Read(bs)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+		fmt.Printf("read: %s\n", string(bs))
+	}
+
 }
