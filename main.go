1df78d2ab93a3f48533017225537a01186a0a147
Waterbootdev <wassermann.sp@gmail.com>
2025-08-08 10:58:30 +0200

    read file to channel

--- main.go
+++ main.go
@@ -2,46 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"strings"
 )
 
-func readLines() {
+func main() {
 	file, err := os.Open("message.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("err: %v\n", err)
+		return
 	}
+
 	defer file.Close()
-	line := ""
 
-	for {
-		bs := make([]byte, 8)
-		_, err = file.Read(bs)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println(err)
-			}
-			break
-		}
+	lines := getLinesChannel(file)
 
-		parts := strings.Split(string(bs), "\n")
-
-		if len(parts) > 1 {
-			line += parts[0]
-			fmt.Printf("read: %s\n", line)
-			line = parts[1]
-		} else {
-			line += parts[0]
-		}
-	}
-
-	if len(line) > 0 {
+	for line := range lines {
 		fmt.Printf("read: %s\n", line)
-		line = ""
 	}
-}
 
-func main() {
-	readLines()
 }
